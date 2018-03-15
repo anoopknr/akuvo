ef6e612d61a64665cd23d5c2747d8377bc4bb864
anto bose <antobose@cs.ajce.in>
2018-03-15 16:16:05 +0530

    Modification On UserDetailsActivity.java

--- app/src/main/java/com/example/root/akuvo/UserDetailsActivity.java
+++ app/src/main/java/com/example/root/akuvo/UserDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.root.akuvo;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,63 +8,69 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileOutputStream;
+import java.util.Calendar;
 
 public class UserDetailsActivity extends AppCompatActivity {
 
-    EditText editText,editText3;
+    EditText name,email;
     Button b_save;
-    TextView textView,textView2,textView3,textView4,textView5;
-    Spinner spinner,spinner2,spinner3,spinner4;
+    TextView bloodgrp,dob;
+    EditText mDateEditText;
+    Calendar mCurrentDate;
+    Spinner spinner;
+    String DateOfBirth;
 
     ArrayAdapter<CharSequence> adapter;
-    ArrayAdapter <CharSequence> adapter1;
-    ArrayAdapter <CharSequence> adapter2;
-    ArrayAdapter <CharSequence> adapter3;
     String fileName = "UserDetails.dat";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
-        editText = (EditText) findViewById(R.id.editText);
-        editText3 = (EditText) findViewById(R.id.editText3);
-        textView = (TextView) findViewById(R.id.textView);
-        textView5 = (TextView) findViewById(R.id.textView5);
+        name = (EditText) findViewById(R.id.Name);
+        email = (EditText) findViewById(R.id.Email);
+        bloodgrp = (TextView) findViewById(R.id.BloodGrp);
+        dob = (TextView) findViewById(R.id.DateOfBirth);
         spinner = (Spinner) findViewById(R.id.spinner);
-        spinner2 = (Spinner) findViewById(R.id.spinner2);
-        spinner3 = (Spinner) findViewById(R.id.spinner3);
-        spinner4 = (Spinner) findViewById(R.id.spinner4);
 
         b_save = (Button) findViewById(R.id.b_save);
 
         adapter = ArrayAdapter.createFromResource(this,R.array.blood_groups,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        mDateEditText = (EditText) findViewById(R.id.calender);
+        mDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentDate = Calendar.getInstance();
+                int year = mCurrentDate.get(Calendar.YEAR);
+                int month = mCurrentDate.get(Calendar.MONTH);
+                int day = mCurrentDate.get(Calendar.DAY_OF_MONTH);
 
+                DatePickerDialog mDatePicker = new DatePickerDialog(UserDetailsActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int selectedYear, int selectedMonth, int selectedDay) {
+                        mDateEditText.setText(selectedDay+"-"+selectedMonth+"-"+selectedYear);
 
-        adapter1 = ArrayAdapter.createFromResource(this,R.array.date,android.R.layout.simple_spinner_item);
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(adapter1);
-
-
-        adapter2 = ArrayAdapter.createFromResource(this,R.array.month,android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner3.setAdapter(adapter2);
-
-
-        adapter3 = ArrayAdapter.createFromResource(this,R.array.year,android.R.layout.simple_spinner_item);
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner4.setAdapter(adapter3);
+                        mCurrentDate.set(selectedYear,selectedMonth,selectedDay);
+                    }
+                }, year, month, day);
+                mDatePicker.show();
+                DateOfBirth=day+"-"+month+"-"+year;
+            }
+        });
 
         b_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                saveFile(fileName, editText.getText().toString()+"\n"+spinner2.getSelectedItem().toString()+"/"+spinner3.getSelectedItem().toString()+"/"+spinner4.getSelectedItem().toString()+"\n"+spinner.getSelectedItem().toString()+"\n"+editText3.getText().toString());
+
+                saveFile(fileName, name.getText().toString()+"\n"+DateOfBirth+"\n"+spinner.getSelectedItem().toString()+"\n"+email.getText().toString());
                 startActivity(new Intent(UserDetailsActivity.this,MainActivity.class));
             }
         });
